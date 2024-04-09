fc35d96d3727107de8c3af3e95f904fe380e1fe9
Kamran <56656396+Kamran-B@users.noreply.github.com>
2024-04-08 23:41:47 -0700

    Barrels tested locally

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -1,3 +1,5 @@
+import sqlalchemy
+from src import database as db
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
@@ -21,6 +23,19 @@ class Barrel(BaseModel):
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     """ """
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
+    newml = 0
+    cost = 0
+
+    with db.engine.begin() as connection:
+        print("gold tuple:", connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).fetchone())
+        currentgold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).fetchone()[0]
+        for barrel in barrels_delivered:
+            newml += barrel.ml_per_barrel
+            cost += barrel.price
+        
+        print("NEW ML:", newml)
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = num_green_ml + :new"), {'new': newml})
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = gold - :costval"), {'costval': cost})
 
     return "OK"
 
@@ -29,11 +44,15 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """ """
     print(wholesale_catalog)
+    plan = []
+    with db.engine.begin() as connection:
+        print("gold tuple:", connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).fetchone())
+        currentgold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).fetchone()[0]
+        currentpotions = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).fetchone()[0]
+        for barrel in wholesale_catalog:
+            if currentpotions < 10 and "GREEN" in barrel.sku and barrel.price <= currentgold:
+                plan.append({"sku": barrel.sku, "quantity": 1})
+                break
 
-    return [
-        {
-            "sku": "SMALL_RED_BARREL",
-            "quantity": 1,
-        }
-    ]
+    return plan
 
