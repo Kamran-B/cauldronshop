9e3a1e32c99876bd977b29d15df160489ece5b45
Kamran <56656396+Kamran-B@users.noreply.github.com>
2024-04-15 15:50:36 -0700

    Adding red and blue potions

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -10,20 +10,36 @@ def get_catalog():
     """
     Each unique item combination must have only a single price.
     """
+    catalog = []
     with db.engine.begin() as connection:
-        row = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory")).fetchone()
+        green = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).fetchone()[0]
+        red = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory")).fetchone()[0]
+        blue = connection.execute(sqlalchemy.text("SELECT num_blue_potions FROM global_inventory")).fetchone()[0]
     
-    print(row)
 
-    if row[0] == 0:
-        return []
-
-    return [
-            {
+    if green > 0:
+        catalog.append({
                 "sku": "GREEN_POTION_0",
                 "name": "green potion",
-                "quantity": 1, #row[0],
+                "quantity": 1,
                 "price": 50,
                 "potion_type": [0, 1, 0, 0],
-            }
-        ]
+            })
+    if red > 0:
+        catalog.append({
+                "sku": "RED_POTION_0",
+                "name": "red potion",
+                "quantity": 1,
+                "price": 50,
+                "potion_type": [1, 0, 0, 0],
+            })
+    if blue > 0:
+        catalog.append({
+                "sku": "BLUE_POTION_0",
+                "name": "blue potion",
+                "quantity": 1,
+                "price": 50,
+                "potion_type": [0, 0, 1, 0],
+            })
+
+    return catalog
