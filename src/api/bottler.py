a18453d34520281ad7470e1eece5b419fa5055bc
Kamran <56656396+Kamran-B@users.noreply.github.com>
2024-04-15 15:51:09 -0700

    Adding red and blue

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -45,17 +45,28 @@ def get_bottle_plan():
 
     newpotions = 0
     with db.engine.begin() as connection:
-        currentml = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).fetchone()[0]
-        newpotions = currentml // 100
+        currentGreenMl = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).fetchone()[0]
+        currentRedMl = connection.execute(sqlalchemy.text("SELECT num_red_ml FROM global_inventory")).fetchone()[0]
+        currentBlueMl = connection.execute(sqlalchemy.text("SELECT num_blue_ml FROM global_inventory")).fetchone()[0]
+        newGreenPotions = currentGreenMl // 100
+        newRedPotions = currentRedMl // 100
+        newBluePotions = currentBlueMl // 100
         plan = []
-        #newpotions += currentml // 100
-        #currentml -= 100 * (currentml // 100)
-        #connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = :currentml"), {'currentml': currentml})
-        #connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = num_green_potions + :newpotions"), {'newpotions': newpotions})
-        if newpotions > 0:
+        
+        if newGreenPotions > 0:
+            plan.append({
+                    "potion_type": [0, 1, 0, 0],
+                    "quantity": newGreenPotions,
+                })
+        if newRedPotions > 0:
+            plan.append({
+                    "potion_type": [1, 0, 0, 0],
+                    "quantity": newRedPotions,
+                })
+        if newBluePotions > 0:
             plan.append({
                     "potion_type": [0, 0, 1, 0],
-                    "quantity": newpotions,
+                    "quantity": newBluePotions,
                 })
 
         return plan
