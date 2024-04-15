00b44710af3ccde61c6c188110f710e74dff405d
Kamran <56656396+Kamran-B@users.noreply.github.com>
2024-04-15 15:51:30 -0700

    Adding red and blue

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -23,18 +23,26 @@ class Barrel(BaseModel):
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     """ """
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
-    newml = 0
+    newGreenMl = 0
+    newRedMl = 0
+    newBlueMl = 0
     cost = 0
 
     with db.engine.begin() as connection:
         print("gold tuple:", connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).fetchone())
         currentgold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).fetchone()[0]
         for barrel in barrels_delivered:
-            newml += barrel.ml_per_barrel
+            if barrel.potion_type == [0, 1, 0, 0]:
+                newGreenMl += barrel.ml_per_barrel
+            if barrel.potion_type == [1, 0, 0, 0]:
+                newRedMl += barrel.ml_per_barrel
+            if barrel.potion_type == [0, 0, 1, 0]:
+                newBlueMl += barrel.ml_per_barrel
             cost += barrel.price
         
-        print("NEW ML:", newml)
-        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = num_green_ml + :new"), {'new': newml})
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = num_green_ml + :new"), {'new': newGreenMl})
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = num_green_ml + :new"), {'new': newRedMl})
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_ml = num_green_ml + :new"), {'new': newBlueMl})
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = gold - :costval"), {'costval': cost})
 
     return "OK"
@@ -51,7 +59,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         currentpotions = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).fetchone()[0]
         for barrel in wholesale_catalog:
             print("barrel sku:", barrel.sku, "current potions:", currentpotions)
-            if currentpotions < 10 and barrel.potion_type == [0, 1, 0, 0] and barrel.price <= currentgold:
+            if currentpotions < 10 and (barrel.potion_type in [[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 1, 0]]) and barrel.price <= currentgold:
                 plan.append({"sku": barrel.sku, "quantity": 1})
                 print("     buying barrel")
                 break
