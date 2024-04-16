ea3b3ceeabbd57cc2b35a1768dd840f538f0da46
Kamran <56656396+Kamran-B@users.noreply.github.com>
2024-04-15 22:26:37 -0700

    Adding reset

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,3 +1,5 @@
+import sqlalchemy
+from src import database as db
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
@@ -14,5 +16,15 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = 0"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = 0"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = 0"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = 0"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_potions = 0"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blie_ml = 0"))
+
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = 100"))
+
     return "OK"
 
