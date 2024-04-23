cae35158e55a5dae0e9a981cc459c1858f3fe489
Kamran <56656396+Kamran-B@users.noreply.github.com>
2024-04-22 21:28:27 -0700

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,41 @@
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    character_class TEXT,
+    level INT4
+);
+
+CREATE TABLE carts (
+    id SERIAL PRIMARY KEY,
+    customer_id INT4 REFERENCES customers(id)
+);
+
+CREATE TABLE potions (
+    id SERIAL PRIMARY KEY,
+    sku TEXT,
+    name TEXT,
+    quantity INT4,
+    price INT4
+);
+
+CREATE TABLE cart_items (
+    cart_id INT4 REFERENCES carts(id),
+    potion_id INT4 REFERENCES potions(id),
+    quantity INT4,
+    PRIMARY KEY (cart_id, potion_id)
+);
+
+CREATE TABLE global_inventory (
+    id SERIAL PRIMARY KEY,
+    gold INT4,
+    num_red_ml INT4,
+    num_green_ml INT4,
+    num_blue_ml INT4,
+    num_dark_ml INT4
+);
+
+CREATE TABLE processed (
+    id SERIAL PRIMARY KEY,
+    job_id INT4,
+    type TEXT
+);
