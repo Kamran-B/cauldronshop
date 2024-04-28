89d986c396340dbd1500c350fe8f5caa5161d5db
Kamran <56656396+Kamran-B@users.noreply.github.com>
2024-04-28 11:07:43 -0700

    fixing initial values for potions

--- schema.sql
+++ schema.sql
@@ -14,6 +14,7 @@ CREATE TABLE potions (
     id SERIAL PRIMARY KEY,
     sku TEXT,
     name TEXT,
+    type TEXT,
     quantity INT4,
     price INT4
 );
@@ -43,4 +44,26 @@ CREATE TABLE processed (
 );
 
 INSERT INTO global_inventory (gold, num_red_ml, num_green_ml, num_blue_ml, num_dark_ml, potion_capacity, ml_capacity) 
-VALUES (100, 0, 0, 0, 0, 50, 10000)
+VALUES (100, 0, 0, 0, 0, 50, 10000);
+
+    
+INSERT INTO potions (sku, name, type, quantity, price) 
+VALUES ('RED_POTION_0', 'red potion', '[100, 0, 0, 0]', 0, 40);
+
+INSERT INTO potions (sku, name, type, quantity, price) 
+VALUES ('GREEN_POTION_0', 'green potion', '[0, 100, 0, 0]', 0, 40);
+    
+INSERT INTO potions (sku, name, type, quantity, price) 
+VALUES ('BLUE_POTION_0', 'blue potion', '[0, 0, 100, 0]', 0, 40);
+    
+INSERT INTO potions (sku, name, type, quantity, price) 
+VALUES ('DARK_POTION_0', 'dark potion', '[0, 0, 0, 100]', 0, 40);
+
+INSERT INTO potions (sku, name, type, quantity, price) 
+VALUES ('PURPLE_POTION_0', 'purple potion', '[50, 0, 50, 0]', 0, 45);
+
+INSERT INTO potions (sku, name, type, quantity, price) 
+VALUES ('YELLOW_POTION_0', 'yellow potion', '[50, 50, 0, 0]', 0, 50);
+
+INSERT INTO potions (sku, name, type, quantity, price) 
+VALUES ('WHITE_POTION_0', 'white potion', '[34, 33, 33, 0]', 0, 60);
