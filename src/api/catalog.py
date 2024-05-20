2c85e91eb531a6808d1b417c20675daa31bdde54
Kamran <kmbastani@yahoo.com>
2024-05-19 23:37:26 -0700

    forgot to start selling red and dark again

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -21,9 +21,7 @@ def get_catalog():
         print("Current inventory:", stock)
 
         for potion in stock:
-            if potion[0] in ["RED_POTION_0", "DARK_POTION_0"]:
-                continue
-            
+
             if potion[2] > 0:
                 catalog.append({
                 "sku": potion[0],
